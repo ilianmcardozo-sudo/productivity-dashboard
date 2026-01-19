ed02aabe20eab527bde8a89b73b426c08a9adc79
fede <federicopirabanloaiza@gmail.com>
2026-01-19 09:50:26 -0500

    mejora del to do

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,42 @@
 import {useState} from "react";
 
 export default function Home() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<{id:Number, text:string}[]>([]);
   const [input, setInput] = useState("");
   
   const addTask = () => {
     if (!input.trim()) return;
-    setTasks([...tasks, input]);
+    setTasks([...tasks,{id:Date.now(), text:input}]);
     setInput("");
+    const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        addTask();
+      }
+      const deleteTask = (id: Number) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+      }
   }
-   return (
-  <main style={{ padding: "2rem" }}>
-    <h1>Lista de tareas</h1>
+  return (
+  <main className="container">
+    <h1> Lista de Tareas </h1>
 
-    <input
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      placeholder="Escribe una tarea"
-    />
-
-    <button onClick={addTask}>Agregar</button>
+    <div className="input-group">
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder="Escribe una tarea y presiona Enter"
+      />
+      <button onClick={addTask}>Agregar</button>
+    </div>
 
     <ul>
-      {tasks.map((task, index) => (
-        <li key={index}>{task}</li>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <span>{task.text}</span>
+          <button onClick={() => deleteTask(task.id)}>X</button>
+        </li>
       ))}
     </ul>
   </main>
-);}
+);}}
